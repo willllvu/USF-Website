0bae814a25dbf86e46237025fd0f01a2bab35745
twitchyraccoon <twitchyraccoon@gmail.com>
2019-04-03 23:22:57 -0400

    added navigation bar function

--- USF-Javascript.js
+++ USF-Javascript.js
@@ -1,18 +1,17 @@
 // slideshow business
-var i; //controls when the slides disappear
 var l = 0; //controls when the slides appear
 slideshowme();
+var numberslide = document.getElementsByClassName("slides");//defines the slides
 function slideshowme() {
-    var numberslide = document.getElementsByClassName('slides');//defines the slides
-    
-    for (let i = 0; i < i.length; i++) {
+    var i; //controls when the slides disappear
+
+    for (i = 0; i < numberslide.length; i++) {
         numberslide[i].style.display = 'none';
     }//counts and causes the slides to DISAPPEAR!
     l++
     if (l > numberslide.length) {
         l = 1;
     }
-    numberslide[l - 1].style.display = 'block';
-setTimeout(slideshowme, 3000);
-
+    numberslide[l - 1].style.display = "block";
+    setTimeout(slideshowme, 1000);
 }
