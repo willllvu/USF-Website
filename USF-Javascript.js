09ce9a363432272b492a6032f501fd2e10529cad
twitchyraccoon <twitchyraccoon@gmail.com>
2019-04-04 19:32:01 -0400

    the slideshow works now and im happy

--- USF-Javascript.js
+++ USF-Javascript.js
@@ -1,16 +1,25 @@
 // slideshow business
-var i;
 var l = 0;
 function homepageslideshow() {
-     editslides = document.getElementsByClassName('slides');
-    
-     for (let i = 0; i < editslides.length; i++) {
-        editslides.style.display = 'none';
-    }
-    l++;
-    if (l > editslides) {
-        l = 0;
-    }
-    editslides[l-1].style.display = 'block';
+    editslides = document.getElementsByClassName('slides');
+
+    // for (let i = 0; i < editslides.length; i++) {
+    //     editslides.style.display = 'none';
+    // }
+    // l++;
+    // if (l > editslides.length) {
+    //     l = 0;
+    // }
+    // editslides[l - 1].style.display = 'block';
+    // setTimeout(homepageslideshow, 2000);
+        for (let i = 0; i < editslides.length; i++) {
+            if (i === l) editslides[l].style.display = 'block';
+            else editslides[i].style.display = 'none';
+            console.log(i + " " + editslides[i]);
+            console.log(l);
+        }
+        l++;
+        if (l === editslides.length) l = 0;
+        setTimeout(homepageslideshow, 2000);
 }
 homepageslideshow();
