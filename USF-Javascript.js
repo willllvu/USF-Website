8c4d3910d4c157ca43c1075aca0a6e2f016c3d74
twitchyraccoon <twitchyraccoon@gmail.com>
2019-04-06 21:13:29 -0400

    javascript chagnes

--- USF-Javascript.js
+++ USF-Javascript.js
@@ -2,16 +2,6 @@
 var l = 0;
 function homepageslideshow() {
     editslides = document.getElementsByClassName('slides');
-
-    // for (let i = 0; i < editslides.length; i++) {
-    //     editslides.style.display = 'none';
-    // }
-    // l++;
-    // if (l > editslides.length) {
-    //     l = 0;
-    // }
-    // editslides[l - 1].style.display = 'block';
-    // setTimeout(homepageslideshow, 2000);
         for (let i = 0; i < editslides.length; i++) {
             if (i === l) editslides[l].style.display = 'block';
             else editslides[i].style.display = 'none';
